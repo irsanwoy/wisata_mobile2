53402bc474b2af81d58094374615ac5e6bdcdec4
irsanwoy <irsanwoy54@gmail.com>
2024-11-23 13:51:07 +0700

    pertemuan7

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan6.dart';
+import 'package:aslab_travel/screens/homepage.dart';
+import 'package:aslab_travel/screens/landingpage.dart';
+import 'package:aslab_travel/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
